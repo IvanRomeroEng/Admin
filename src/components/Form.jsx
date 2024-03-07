9f4403d863374d42c8515a27f5dd7fb75272741d
Ivan M Romero <Ivan.Romero.Eng@outlook.com>
2024-03-06 21:47:17 -0400

    colocando el formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div className='md:w-1/2 lg:w-2/5'>
+    <div className='md:w-1/2 lg:w-2/5 mx-10'>
       <h2 className='font-black text-3xl text-center'>Seguimiento Paciente</h2>
 
       <p className='text-lg mt-5 text-center mb-10'>
@@ -12,10 +12,13 @@ const Form = () => {
 
       <form className= 'bg-white shadow-md rounded-lg py-10 px-5'>
         <div>
-          <label className='block '>Nombre Mascotas</label>
+          <label htmlFor="nombrePaciente" className='block '>Nombre del Paciente
+          </label>
           <input 
+          id='nombrePaciente'
           type="text"
-          placeholder='Nombre del Paciente' 
+          placeholder='Nombre del Paciente'
+          className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
           />
         </div>
       </form>
